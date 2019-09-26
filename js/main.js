2d40d0a5ae28d3d6bbf386ff323cd842abea2f9d
Alexander MacOS <Frost1k1987@gmail.com>
2019-09-26 03:44:32 +0300

    temp 1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.sl').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,    
+    adaptiveHeight: true,
+    prevArrow: "<img src='https://svgshare.com/i/6Ei.svg' class='prev' alt='1'>",
+    nextArrow: "<img src='https://svgshare.com/i/6Gf.svg' class='next' alt='2'>",
+    cssEase: 'linear'
+});
