90bddecd6cac598820b617c8119c4e910c2f9c14
Alexander MacOS <Frost1k1987@gmail.com>
2019-10-06 15:36:41 +0300

    footer 1

--- js/main.js
+++ js/main.js
@@ -2,9 +2,35 @@ $('.sl').slick({
     dots: true,
     infinite: true,
     speed: 500,
-    fade: true,    
+    fade: true,
     adaptiveHeight: true,
     prevArrow: "<img src='https://svgshare.com/i/6Ei.svg' class='prev' alt='1'>",
     nextArrow: "<img src='https://svgshare.com/i/6Gf.svg' class='next' alt='2'>",
     cssEase: 'linear'
+});
+$('.monials-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    arrows: false,
+    adaptiveHeight: true,
+    cssEase: 'linear'
+});
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    swipe: false,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    infinite: true,
+    swipe: false,
+    focusOnSelect: true
 });
