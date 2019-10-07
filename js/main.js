5f4fe394f039f474f5d089d90c9665a2af25c538
Alexander MacOS <Frost1k1987@gmail.com>
2019-10-07 16:05:54 +0300

    mail 3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+$('form').submit(function (event) {
+    event.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "/mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function () {
+        $(this).find("input").val("");
+        alert("Сообщение успешно отправлено");
+        $("form").trigger("reset");
+        $('.modal').modal('hide');
+    });
+    return false;
+});
 $('.sl').slick({
     dots: true,
     infinite: true,
